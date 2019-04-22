8395540d1c22d86ea62d815a20925d2bc9c1a116
Jim Hough <teachlife@outlook.com>
2019-04-22 01:39:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,10 @@ def line (katz_deli)
   if katz_deli.length == 0
     return "The line is currently empty."
   else
-    list = array.map.with_index{|x , i| (i+1).to_s +  '. ' + x}
+    list = katz_deli.map.with_index{|x , i| (i+1).to_s +  '. ' + x}
     return "The line is currently: "<< list.join(" ")
+end
+
+def take_a_number (katz_deli, name)
+  return "Welcome #{name}, "
 end
