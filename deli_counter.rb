cf5c83af8211e8fbe55faf0b076b341161d748fa
Jim Hough <teachlife@outlook.com>
2019-04-22 01:36:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,7 @@ katz_deli = []
 def line (katz_deli)
   if katz_deli.length == 0
     return "The line is currently empty."
-  else 
+  else
     list = array.map.with_index{|x , i| (i+1).to_s +  '. ' + x}
     return "The line is currently: "<< list.join(" ")
 end
-
-
